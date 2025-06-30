382d34d4d72beb24964904c3291392897a7b0912
Florian Neise <neiflm22@htl-kaindorf.at>
2025-06-30 16:54:40 +0200

    lokaler https Webserver mit express, ignoriere node_modules

---
+++ web-entwicklung/mtg-mini-website/main.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+import fs from'node:fs';
+import https from 'node:https';
+
+const app = express()
+
+app.use(express.static('/home/florian/flo-self-study/web-entwicklung/mtg-mini-website'));
+
+app.get('/', (req, res) => {
+  res.sendFile('/home/florian/flo-self-study/web-entwicklung/mtg-mini-website/index.html');
+})
+
+const options = {
+	cert: fs.readFileSync('flo.crt'),
+	key: fs.readFileSync('flo.pem'),
+	ciphers: "TLS_AES_128_GCM_SHA256"
+}
+
+const server = https.createServer(options, app).listen(4000);
+console.log('Web-Server gestartet: https://localhost:4000');
