f520d06c2ccb39a9d3e9684a22b6f5e8bef1c4ef
Florian Neise <neiflm22@htl-kaindorf.at>
2025-08-27 22:01:25 +0200

    start of mastermind

---
+++ web-entwicklung/mastermind/script.js
@@ -0,0 +1,14 @@
+var code;
+const colorPool = ["red", "yellow", "orange", "green", "blue", "purple"];
+
+window.onload = function() {
+    generateCode();
+}
+
+function generateCode() {
+    var colorsLeft = colorPool;
+    for (let i = 0; i < 5; i++) {
+        let index = Math.floor(Math.random() * colorPool.length);
+        
+    }
+}
